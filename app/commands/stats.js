94309b082a16c61fc4ea618e2efb0676e4877165
k03mad <k03.mad@gmail.com>
2017-12-09 18:31:11 +0300

    cleanup stats

--- app/commands/stats.js
+++ app/commands/stats.js
@@ -1,48 +1,87 @@
 const exec = require('executive');
 
 /**
- * Run command at bash
+ * Get RPi3 GPU temperature
+ *
+ * bash log:
+ * temp=50.5'C
  */
-const run = async command => {
-    const cmd = await exec(command);
-    return cmd.stdout;
+const gpuTemp = async () => {
+    const gpu = await exec('vcgencmd measure_temp');
+    return `GPU temp: ${
+        gpu.stdout
+            .replace('temp=', '')
+            .replace('\'', '°')
+    }`;
+};
+
+/**
+ * Get RPi3 CPU temperature
+ *
+ * bash log:
+ * temp=50.5'C
+ */
+const cpuTemp = async () => {
+    const cpu = await exec('cat /sys/class/thermal/thermal_zone0/temp');
+    return `CPU temp: ${
+        (Number(cpu.stdout) / 1000).toFixed(1)
+    }°C\n\n`;
+};
+
+/**
+ * Get RPi3 CPU usage
+ *
+ * bash log:
+ * cpu  22118 0 3669 658313 3087 0 119 0 0 0
+ */
+const cpuUsage = async () => {
+    let cpu = await exec('grep \'cpu \' /proc/stat');
+    cpu = cpu.split(' ').map(elem => Number(elem));
+    cpu = (cpu[2] + cpu[4]) * 100 / (cpu[2] + cpu[4] + cpu[5]);
+    return `CPU usage: ${
+        cpu.toFixed(2)
+    }%\n`;
+};
+
+/**
+ * Get RPi3 RAM usage
+ *
+ * bash log:
+ *                 total        used        free      shared  buff/cache   available
+ * Mem:            923          79         484          11         359         778
+ */
+const ramUsage = async () => {
+    const ram = await exec('free -m');
+    const [, ramTotal] = ram.match(/Mem: +(\d+)/);
+    const [, ramUsed] = ram.match(/Mem: +\d+ +(\d+)/);
+    return `RAM usage: ${ramUsed}MB/${ramTotal}MB\n`;
+};
+
+/**
+ * Get RPi3 disk usage
+ *
+ * bash log:
+ * Файловая система Размер Использовано  Дост Использовано% Cмонтировано в
+ * /dev/root           57G         5,4G   49G           10% /
+ */
+const diskUsage = async () => {
+    const disk = await exec('df -h');
+    const [, diskTotal] = disk.match(/\/dev\/root +(\d+G)/);
+    const [, diskUsed] = disk.match(/\/dev\/root +\d+. +([\d,]+.)/);
+    return `Disk usage: ${diskUsed}B/${diskTotal}B`;
 };
 
 /**
  * Get some stats
  */
 const getStats = async () => {
-    const stats = [];
-
-    // temp=50.5'C
-    const gpuTemp = await run('vcgencmd measure_temp');
-    stats.push(`GPU temp: ${gpuTemp.replace('temp=', '').replace('\'', '°')}`);
-
-    // 51002
-    const cpuTemp = await run('cat /sys/class/thermal/thermal_zone0/temp');
-    stats.push(`CPU temp: ${(Number(cpuTemp) / 1000).toFixed(1)}°C\n\n`);
-
-    // cpu  22118 0 3669 658313 3087 0 119 0 0 0
-    let cpuUsage = await run('grep \'cpu \' /proc/stat');
-    cpuUsage = cpuUsage.split(' ').map(elem => Number(elem));
-    cpuUsage = (cpuUsage[2] + cpuUsage[4]) * 100 / (cpuUsage[2] + cpuUsage[4] + cpuUsage[5]);
-    stats.push(`CPU usage: ${cpuUsage.toFixed(2)}%\n`);
-
-    //                 total        used        free      shared  buff/cache   available
-    // Mem:            923          79         484          11         359         778
-    const ramUsage = await run('free -m');
-    const [, ramTotal] = ramUsage.match(/Mem: +(\d+)/);
-    const [, ramUsed] = ramUsage.match(/Mem: +\d+ +(\d+)/);
-    stats.push(`RAM usage: ${ramUsed}MB/${ramTotal}MB\n`);
-
-    // Файловая система Размер Использовано  Дост Использовано% Cмонтировано в
-    // /dev/root           57G         5,4G   49G           10% /
-    const diskUsage = await run('df -h');
-    const [, diskTotal] = diskUsage.match(/\/dev\/root +(\d+G)/);
-    const [, diskUsed] = diskUsage.match(/\/dev\/root +\d+. +([\d,]+.)/);
-    stats.push(`Disk usage: ${diskUsed}B/${diskTotal}B`);
-
-    return stats.join('');
+    return [
+        await gpuTemp(),
+        await cpuTemp(),
+        await cpuUsage(),
+        await ramUsage(),
+        await diskUsage()
+    ].join(' ');
 };
 
 module.exports = getStats;
