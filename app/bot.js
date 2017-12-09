68e9d13d792a6cea08aaefb6cca34a7dbddcb6ff
k03mad <k03.mad@gmail.com>
2017-12-09 20:28:04 +0300

    add run command

--- app/bot.js
+++ app/bot.js
@@ -2,7 +2,7 @@ const TelegramBot = require('node-telegram-bot-api');
 const c = require('require-all')(`${__dirname}/commands`);
 const {telegramToken, myChat} = require('./lib/env');
 const {msg} = require('./lib/messages');
-const {sendText, sendMdText, q} = require('./lib/senders');
+const {search, sendText, sendMdText, q} = require('./lib/senders');
 
 const bot = new TelegramBot(telegramToken, {polling: {
     interval: 3000,
@@ -13,6 +13,7 @@ bot.sendMessage(myChat, msg.common.deployed);
 
 /* eslint-disable no-multi-spaces, func-call-spacing, space-in-parens */
 
-bot.onText(  q('help|start'),         mes => sendText      (bot, mes,         c.help('bot')  ));
-bot.onText(  q('reboot'),             ()  =>                                  c.reboot()      );
-bot.onText(  q('stats'),        async mes => sendMdText    (bot, mes,   await c.stats()      ));
+bot.onText(  q('help|start'),         mes => sendText      (bot, mes,         c.help('bot')      ));
+// bot.onText(  q('reboot'),             ()  =>                                  c.reboot()      );
+bot.onText(  q('run', 'search'),      mes => sendText      (bot, mes,         c.run(search(mes)) ));
+bot.onText(  q('stats'),        async mes => sendMdText    (bot, mes,   await c.stats()          ));
