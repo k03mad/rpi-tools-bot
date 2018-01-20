1c9e4a866a6addd0e6a783aee40498e99c23c7c2
k03mad <k03.mad@gmail.com>
2018-01-21 01:04:03 +0300

    add wifi

---
+++ app/commands/wifi.js
@@ -0,0 +1,38 @@
+const scanner = require('node-wifi-scanner');
+
+/**
+ * Scan wifi
+ */
+const scan = () => {
+    return new Promise((resolve, reject) => {
+        scanner.scan((err, networks) => {
+            err ? reject(err) : resolve(networks);
+        });
+    });
+};
+
+scan().then(console.log);
+
+/**
+ * Get local device list
+ */
+const arp = async () => {
+    const devices = [];
+    const scanned = await scan();
+
+    scanned
+        .filter(elem => elem.alive)
+        .forEach(elem => {
+            for (const key in elem) {
+                if (key !== 'alive' && elem[key] && !elem[key].includes('Error')) {
+                    devices.push(`${key}: ${elem[key]}`);
+                }
+            }
+
+            devices.push('');
+        });
+
+    return devices.join('\n');
+};
+
+module.exports = arp;
