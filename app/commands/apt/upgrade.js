1cd83def779457f24bc223e2b2c74cbaf3759f6d
k03mad <k03.mad@gmail.com>
2018-03-06 01:10:03 +0300

    sml fix

--- app/commands/apt/upgrade.js
+++ app/commands/apt/upgrade.js
@@ -10,9 +10,7 @@ const upgrades = async () => {
         'sudo apt-get update',
         'sudo apt-get upgrade -y',
         'sudo apt-get autoremove -y',
-        'sudo apt-get autoclean',
-        'npm i -g npm',
-        'npm i -g forever'
+        'sudo apt-get autoclean'
     ];
 
     for (const cmd of commands) {
