307cd39f189f4e3439f807dc7646549bd5ea7239
k03mad <k03.mad@gmail.com>
2018-03-17 00:36:02 +0300

    wtf

--- my.py
+++ my.py
@@ -8,15 +8,16 @@ def mh_z19():
         bytesize=serial.EIGHTBITS,
         parity=serial.PARITY_NONE,
         stopbits=serial.STOPBITS_ONE,
-        timeout=1.0,
-        )
+        timeout=1.0
+    )
+
     while 1:
-        result = ser.write("\xff\x01\x86\x00\x00\x00\x00\x00\x79")
-        s = ser.read(9)
-        if s[0] == '\xff' and s[1] == '\x86':
-            return {'co2': ord(s[2]) * 256 + ord(s[3])}
+        result=ser.write("\xff\x01\x86\x00\x00\x00\x00\x00\x79")
+        s=ser.read(9)
+        if s[0] == "\xff" and s[1] == "\x86":
+            return {'co2': ord(s[2])*256 + ord(s[3])}
         break
 
 if __name__ == '__main__':
     value = mh_z19()
-    print "co2=", value['co2']
+    print "co2=", value["co2"]
