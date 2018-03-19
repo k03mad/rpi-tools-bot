66f3add0201b6d1f7940d5ed9b79847dced01741
k03mad <k03.mad@gmail.com>
2018-03-20 02:52:30 +0300

    sml py script fix

--- app/py/ppm.py
+++ app/py/ppm.py
@@ -3,20 +3,18 @@ import serial
 def mh_z19():
     ser = serial.Serial(
         '/dev/serial0',
-        baudrate=9600,
-        bytesize=serial.EIGHTBITS,
-        parity=serial.PARITY_NONE,
-        stopbits=serial.STOPBITS_ONE,
-        timeout=1.0
+        baudrate = 9600,
+        bytesize = serial.EIGHTBITS,
+        parity = serial.PARITY_NONE,
+        stopbits = serial.STOPBITS_ONE,
+        timeout = 1.0
     )
 
     while 1:
-        result=ser.write("\xff\x01\x86\x00\x00\x00\x00\x00\x79")
-        s=ser.read(9)
-        if s[0] == "\xff" and s[1] == "\x86":
-            return {'co2': ord(s[2])*256 + ord(s[3])}
+        result = ser.write('\xff\x01\x86\x00\x00\x00\x00\x00\x79')
+        s = ser.read(9)
+        if s[0] == '\xff' and s[1] == '\x86':
+            return ord(s[2]) * 256 + ord(s[3])
         break
 
-if __name__ == '__main__':
-    value = mh_z19()
-    print value['co2']
+print mh_z19()
