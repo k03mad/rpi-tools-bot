fa2f9c2d2b615fcb919037d4b55b90ab21697cf0
k03mad <k03.mad@gmail.com>
2018-04-01 18:34:22 +0300

    sml fix

--- app/lib/utils.js
+++ app/lib/utils.js
@@ -73,11 +73,11 @@ const getMacVendor = async mac => {
  * Known WiFi-connected devices list
  */
 const KNOWN_DEVICES = {
-    GalaxyS6: 'E8-50-8B-39-55-1D',
-    MacHome: '78-31-C1-CA-63-9E',
-    // MacWork: '8C-85-90-17-69-E7',
-    PhilipsTV: '1C-5A-6B-EA-C1-BB',
-    Nexus5x: 'A8-B8-6E-48-32-D7',
+    S6: 'E8-50-8B-39-55-1D',
+    MacH: '78-31-C1-CA-63-9E',
+    // MacW: '8C-85-90-17-69-E7',
+    TV: '1C-5A-6B-EA-C1-BB',
+    N5x: 'A8-B8-6E-48-32-D7',
     Pi3: 'B8-27-EB-22-57-46'
 };
 
