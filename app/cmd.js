f9d10f0133708886705d325f459c1e67cd5629a8
k03mad <k03.mad@gmail.com>
2018-04-15 00:26:34 +0300

    debug

--- app/cmd.js
+++ app/cmd.js
@@ -11,16 +11,16 @@ const cmd = bot => {
     reply(bot, 'apt_update', c.apt.update());
     reply(bot, 'apt_upgrade', c.apt.upgrade());
 
-    reply(bot, 'pi_sensors', c.pi.sensors(), {parse_mode: 'Markdown'});
-    reply(bot, 'pi_reboot', c.pi.reboot());
-    reply(bot, 'pi_shutdown', c.pi.shutdown());
-    reply(bot, 'pi_stat', c.pi.stat(), {parse_mode: 'Markdown'});
+    // reply(bot, 'pi_sensors', c.pi.sensors(), {parse_mode: 'Markdown'});
+    // reply(bot, 'pi_reboot', c.pi.reboot());
+    // reply(bot, 'pi_shutdown', c.pi.shutdown());
+    // reply(bot, 'pi_stat', c.pi.stat(), {parse_mode: 'Markdown'});
 
-    reply(bot, 'wifi_devices_home', c.wifi.devices());
-    reply(bot, 'wifi_devices_knpl', c.wifi.devices({place: 'knpl'}));
-    reply(bot, 'wifi_reboot_home', c.wifi.reboot());
-    reply(bot, 'wifi_reboot_knpl', c.wifi.reboot({place: 'knpl'}));
-    reply(bot, 'wifi_spots', c.wifi.spots(), {parse_mode: 'Markdown'});
+    // reply(bot, 'wifi_devices_home', c.wifi.devices());
+    // reply(bot, 'wifi_devices_knpl', c.wifi.devices({place: 'knpl'}));
+    // reply(bot, 'wifi_reboot_home', c.wifi.reboot());
+    // reply(bot, 'wifi_reboot_knpl', c.wifi.reboot({place: 'knpl'}));
+    // reply(bot, 'wifi_spots', c.wifi.spots(), {parse_mode: 'Markdown'});
 };
 
 module.exports = cmd;
