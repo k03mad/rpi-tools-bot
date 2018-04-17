ce3f664377f992958b71d742162468daccdfdefe
k03mad <k03.mad@gmail.com>
2018-04-18 01:12:30 +0300

    add bme280

--- app/cmd/pi/sensors.js
+++ app/cmd/pi/sensors.js
@@ -52,4 +52,46 @@ const sensors = async onlyNum => {
     return [message, chart];
 };
 
+const BME280 = require('bme280-sensor');
+
+// The BME280 constructor options are optional.
+// 
+const options = {
+//  i2cBusNo   : 1, // defaults to 1
+  i2cAddress : 0x76
+};
+
+const bme280 = new BME280(options);
+
+// Read BME280 sensor data, repeat
+//
+const readSensorData = async () => {
+  const data = await bme280.readSensorData();
+//    .then((data) => {
+      // temperature_C, pressure_hPa, and humidity are returned by default.
+      // I'll also calculate some unit conversions for display purposes.
+      //
+      data.temperature_F = BME280.convertCelciusToFahrenheit(data.temperature_C);
+      data.pressure_inHg = BME280.convertHectopascalToInchesOfMercury(data.pressure_hPa);
+ 
+      console.log(`data = ${JSON.stringify(data, null, 2)}`);
+   //   setTimeout(readSensorData, 2000);
+   // })
+   // .catch((err) => {
+   //   console.log(`BME280 read error: ${err}`);
+   //   setTimeout(readSensorData, 2000);
+   // });
+};
+
+// Initialize the BME280 sensor
+//
+(async () => {
+
+await bme280.init();
+//  .then(() => {
+  //  console.log('BME280 initialization succeeded');
+    await readSensorData();
+ // })
+ // .catch((err) => console.error(`BME280 initialization failed: ${err} `));
+})();
 module.exports = sensors;
