cc84f6130c6028d37050f57f8466412bd16d0f5f
k03mad <k03.mad@gmail.com>
2018-04-18 01:34:08 +0300

    bme280 progress

--- app/cmd/pi/sensors.js
+++ app/cmd/pi/sensors.js
@@ -1,7 +1,7 @@
 const {getCorlysisChartImage} = require('../../lib/corlysis');
 const {msg} = require('../../lib/messages');
 const {run} = require('../../lib/utils');
-const BME280 = require('bme280-sensor');
+// const BME280 = require('bme280-sensor');
 const path = require('path');
 
 /**
@@ -53,12 +53,27 @@ const sensors = async onlyNum => {
     return [message, chart];
 };
 
-const bme280 = new BME280({i2cAddress: 0x76});
+// const bme280 = new BME280({i2cAddress: 0x76});
 
-(async () => {
-    await bme280.init();
-    const data = await bme280.readSensorData();
-    console.log(data);
-})();
+// (async () => {
+//     await bme280.init();
+//     const data = await bme280.readSensorData();
+//     console.log(data);
+// })();
+
+// const bme = {
+//     temperature_C: 28.39,
+//     humidity: 37.61701977356487,
+//     pressure_hPa: 993.6540132242864
+// };
+
+// bme.pressure = bme.pressure_hPa * 0.75006375541921;
+// delete bme.pressure_hPa;
+
+// for (const elem in bme) {
+//     bme[elem] = Math.round(bme[elem]);
+// }
+
+// console.log(bme);
 
 module.exports = sensors;
