1738a905561260a61633fc04f11421ca86cea49f
Kirill Molchanov <k03.mad@gmail.com>
2018-04-17 11:11:23 +0300

    Update bot.js

--- app/bot.js
+++ app/bot.js
@@ -3,10 +3,11 @@ const {proxyPac, telegramToken} = require('../env');
 const PacProxyAgent = require('pac-proxy-agent');
 const TelegramBot = require('node-telegram-bot-api');
 
+const agent = new PacProxyAgent(proxyPac);
 const bot = new TelegramBot(telegramToken, {polling: {
     interval: 3000,
     params: {allowed_updates: ['message']},
-    request: {agent: new PacProxyAgent(proxyPac)}
+    request: {agent}
 }});
 
 bot.on('polling_error', ex => console.log(msg.common.polling(ex)));
