8b825cf958c28b833137190dde2ff21dd99fd4c5
k03mad <k03.mad@gmail.com>
2018-04-18 22:56:28 +0300

    remove pac

--- app/bot.js
+++ app/bot.js
@@ -1,13 +1,10 @@
 const {msg} = require('./lib/messages');
-const {proxyPac, telegramToken} = require('../env');
-const PacProxyAgent = require('pac-proxy-agent');
+const {telegramToken} = require('../env');
 const TelegramBot = require('node-telegram-bot-api');
 
-const agent = new PacProxyAgent(proxyPac);
 const bot = new TelegramBot(telegramToken, {polling: {
     interval: 3000,
-    params: {allowed_updates: ['message']},
-    request: {agent}
+    params: {allowed_updates: ['message']}
 }});
 
 bot.on('polling_error', ex => console.log(msg.common.polling(ex)));
