de7b9ff8855c2ea1327ac82f935ea333e44ab599
k03mad <k03.mad@gmail.com>
2018-04-23 21:21:36 +0300

    do not send every polling error

--- app/bot.js
+++ app/bot.js
@@ -1,8 +1,9 @@
 const {msg} = require('./lib/messages');
 const {proxy, telegramToken} = require('../env');
+const {sendToCorlysis} = require('./lib/corlysis');
+const moment = require('moment');
 const PacProxyAgent = require('pac-proxy-agent');
 const TelegramBot = require('node-telegram-bot-api');
-const {sendToCorlysis} = require('./lib/corlysis');
 
 const agent = new PacProxyAgent(proxy);
 const bot = new TelegramBot(telegramToken, {
@@ -13,9 +14,17 @@ const bot = new TelegramBot(telegramToken, {
     request: {agent}
 });
 
+const POLLING_REPEAT_ALARM = {time: 60, unit: 'seconds'};
+let pollingTimer = moment();
+
 bot.on('polling_error', err => {
     console.log(msg.common.polling(err));
-    sendToCorlysis('bot=polling', 'pollErr=1i').catch(ex => console.log(msg.chart.cor(ex)));
+
+    // send polling errors to corlysis every 1 minute
+    if (moment().diff(pollingTimer, POLLING_REPEAT_ALARM.unit) > POLLING_REPEAT_ALARM.time) {
+        pollingTimer = moment();
+        sendToCorlysis('bot=polling', 'pollErr=1i').catch(ex => console.log(msg.chart.cor(ex)));
+    }
 });
 
 require('./cmd')(bot);
