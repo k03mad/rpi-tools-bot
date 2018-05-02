41e433748973ad314f13b985301c9bf9b54e4b9b
Kirill Molchanov <k03.mad@gmail.com>
2018-05-02 17:12:19 +0300

    Update reboot.js

--- app/cmd/wifi/reboot.js
+++ app/cmd/wifi/reboot.js
@@ -4,13 +4,13 @@ const {msg} = require('../../lib/messages');
 /**
  * Reboot router
  */
-const reboot = async (opts = {}) => {
-    const host = `http://${router(opts).ip}`;
+const reboot = async place => {
+    const host = `http://${router(place).ip}`;
     const PATH = '/cgi-bin/timepro.cgi?tmenu=background&smenu=reboot&act=&commit=reboot';
 
     try {
         await get(host + PATH, {
-            auth: router(opts).cred
+            auth: router(place).cred
         });
     } catch (ex) {
         return ex.toString();
