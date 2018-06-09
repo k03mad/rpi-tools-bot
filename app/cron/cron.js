28342f12ae465f04e1e60c5814ff711d5ad48286
Kirill Molchanov <k03.mad@gmail.com>
2018-06-09 12:33:31 +0300

    Update cron.js

--- app/cron/cron.js
+++ app/cron/cron.js
@@ -7,16 +7,17 @@ const c = require('require-all')(`${__dirname}/lib`);
  */
 const cron = bot => {
     every('1m').do(() => c.sensorsData(bot));
-    every('1m').do(() => c.statsTemp());
-    every('1m').do(() => c.statsUsage());
     every('1m').do(() => c.yahooData());
 
-    every('2m').do(() => c.wifiDevices(bot));
+    every('1m').do(() => c.statsTemp());
+    every('1m').do(() => c.statsUsage());
+    
+    every('1m').do(() => c.wifiDevices(bot));
 
-    every('5m').do(() => c.dnsQueries());
-    every('5m').do(() => c.dnsTop());
+    every('1m').do(() => c.dnsQueries());
+    every('1m').do(() => c.dnsTop());
 
-    every('15m').do(() => c.lastfm());
+    every('1m').do(() => c.lastfm());
 
     every('1h').do(() => c.adblockErrors());
 
