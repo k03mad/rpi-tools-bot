22bfa41c24f5de3924a2990dd99895da9287b067
k03mad <k03.mad@gmail.com>
2018-06-15 00:42:57 +0300

    update cron time

--- app/cron/cron.js
+++ app/cron/cron.js
@@ -6,24 +6,24 @@ const c = require('require-all')(`${__dirname}/lib`);
  * @param {Object} bot telegram node api
  */
 const cron = bot => {
-    every('1m').do(() => c.sensors.weather(bot));
-    every('1m').do(() => c.yahoo.weather());
-
     every('1m').do(() => c.pi.temp());
     every('1m').do(() => c.pi.usage());
-    every('5h').do(() => c.pi.update(bot));
-
     every('1m').do(() => c.router.devices(bot));
     every('1m').do(() => c.router.traffic());
+    every('1m').do(() => c.sensors.weather(bot));
 
-    every('1m').do(() => c.dns.queries());
-    every('1m').do(() => c.dns.top());
+    every('3m').do(() => c.yahoo.weather());
 
-    every('1m').do(() => c.lastfm.top());
+    every('5m').do(() => c.dns.queries());
+    every('5m').do(() => c.dns.top());
 
-    every('1m').do(() => c.myshows.episodes());
+    every('7m').do(() => c.lastfm.top());
+
+    every('20m').do(() => c.myshows.episodes());
 
     every('1h').do(() => c.adblock.errors());
+
+    every('5h').do(() => c.pi.update(bot));
 };
 
 module.exports = cron;
