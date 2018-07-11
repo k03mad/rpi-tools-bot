1cf264f3a3057a457487f7b4090d1aa0d83a779c
k03mad <k03.mad@gmail.com>
2018-07-12 01:27:10 +0300

    update crons

--- app/cron/cron.js
+++ app/cron/cron.js
@@ -11,20 +11,17 @@ const cron = bot => {
     every('1m').do(() => c.router.devices(bot));
     every('1m').do(() => c.sensors.weather(bot));
 
-    every('3m').do(() => c.router.traffic());
-
     every('5m').do(() => c.air.quality());
     every('5m').do(() => c.dns.queries());
     every('5m').do(() => c.dns.top());
+    every('5m').do(() => c.router.traffic());
 
-    every('7m').do(() => c.lastfm.top());
+    every('10m').do(() => c.lastfm.top());
+    every('10m').do(() => c.yahoo.weather());
 
-    every('15m').do(() => c.yahoo.weather());
+    every('30m').do(() => c.myshows.episodes());
 
-    every('20m').do(() => c.myshows.episodes());
-
-    every('50m').do(() => c.adblock.errors());
-
+    every('1h').do(() => c.adblock.errors());
     every('5h').do(() => c.pi.update(bot));
 };
 
