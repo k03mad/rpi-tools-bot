60938f2601ca249772a7e3161541ca98f100c9bb
k03mad <k03.mad@gmail.com>
2018-08-09 22:09:01 +0300

    sml fix

--- app/cron/lib/dns/domains.js
+++ app/cron/lib/dns/domains.js
@@ -1,4 +1,4 @@
-const {run, sendToInflux} = require('../../../utils');
+const {run, sendToInflux, nowWait} = require('../../../utils');
 const {msg} = require('../../../messages');
 const dnsUpdate = require('../../../bot/lib/commands/dns/update');
 
@@ -10,6 +10,8 @@ const sendDnsDomains = async () => {
 
     try {
         await dnsUpdate();
+        await nowWait(3000);
+
         log = await run('pihole -c -j');
 
         const parsedLog = JSON.parse(log);
