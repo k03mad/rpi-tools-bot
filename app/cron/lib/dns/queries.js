ce590405864d897884497e6a9976da81d81c2e29
k03mad <k03.mad@gmail.com>
2018-09-21 18:33:53 +0300

    add retries to pihole json

--- app/cron/lib/dns/queries.js
+++ app/cron/lib/dns/queries.js
@@ -1,4 +1,4 @@
-const {run, sendToInflux} = require('../../../utils');
+const {run, sendToInflux, nowWait} = require('../../../utils');
 const msg = require('../../../errors');
 
 /**
@@ -6,19 +6,29 @@ const msg = require('../../../errors');
  */
 const sendDnsQueries = async () => {
     let log;
+    let parsedLog;
+    let error;
 
-    try {
-        log = await run('pihole -c -j');
+    // sometimes pihole not returns values
+    for (let i = 0; i < 3; i++) {
+        try {
+            log = await run('pihole -c -j');
+            parsedLog = JSON.parse(log);
+            break;
+        } catch (err) {
+            error = err;
+        }
 
-        const parsedLog = JSON.parse(log);
-        sendToInflux('dns=queries', {
+        await nowWait(2000);
+    }
+
+    error
+        ? console.log(msg.cron.dns(log, error))
+        : sendToInflux('dns=queries', {
             today: parsedLog.dns_queries_today,
             blocked: parsedLog.ads_blocked_today,
             domains: parsedLog.domains_being_blocked,
         });
-    } catch (err) {
-        console.log(msg.cron.dns(log, err));
-    }
 };
 
 module.exports = sendDnsQueries;
