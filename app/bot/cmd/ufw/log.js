baf3a1a50f541b59b9f3fde9b05e2e866c31c903
Kirill Molchanov <k03.mad@gmail.com>
2018-09-29 23:14:55 +0300

    Handle empty ufw log

--- app/bot/cmd/ufw/log.js
+++ app/bot/cmd/ufw/log.js
@@ -3,4 +3,7 @@ const {run} = require('../../../utils');
 /**
  * Get UFW logs
  */
-module.exports = () => run('grep UFW /var/log/syslog');
+module.exports = async () => {
+    const log = await run('grep UFW /var/log/syslog');
+    return log ? log : 'Log is empty';
+};
