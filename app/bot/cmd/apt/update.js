466040b18ef847b36f2ebcf8b4b9feb34961281f
k03mad <k03.mad@gmail.com>
2018-10-23 01:28:23 +0300

    return apt-get

--- app/bot/cmd/apt/update.js
+++ app/bot/cmd/apt/update.js
@@ -8,7 +8,7 @@ const {run} = require('../../../utils');
  * Inst libpoppler64 [0.48.0-2] (0.48.0-2+deb9u1 Raspbian:stable [armhf])
  */
 const update = async () => {
-    const grepUpdates = await run('sudo apt-fast update > /dev/null; apt-fast upgrade -u -s | grep -P "^Inst"');
+    const grepUpdates = await run('sudo apt-get update > /dev/null; apt-get upgrade -u -s | grep -P "^Inst"');
 
     if (grepUpdates) {
         const MSG = 'Updates available:';
--- app/bot/cmd/apt/upgrade.js
+++ app/bot/cmd/apt/upgrade.js
@@ -4,8 +4,8 @@ const {run} = require('../../../utils');
  * Install updates
  */
 module.exports = () => run([
-    'sudo apt-fast update',
-    'sudo apt-fast upgrade -y',
-    'sudo apt-fast autoremove -y',
-    'sudo apt-fast autoclean',
+    'sudo apt-get update',
+    'sudo apt-get upgrade -y',
+    'sudo apt-get autoremove -y',
+    'sudo apt-get autoclean',
 ], {titles: true});
