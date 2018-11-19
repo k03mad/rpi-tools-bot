12c8caf4f2aa0d661e25d21218e60964a682d253
k03mad <k03.mad@gmail.com>
2018-11-20 01:45:30 +0300

    debug

--- app/errors.js
+++ app/errors.js
@@ -3,7 +3,7 @@ const {printMsg} = require('./date');
 module.exports = {
     /* eslint-disable key-spacing, no-multi-spaces */
     cron: {
-        dns:        (log, ex)          => printMsg(`get dns queries:\n${ex}\nlog:\n${log}`),
+        dns:        (log, ex)          => printMsg(`get dns queries:\n${ex}\nlog:\n${[log]}`),
         dnsClients: ex                 => printMsg(`get pihole top clients:\n${ex}`),
         dnsTop:     ex                 => printMsg(`get pihole top hosts:\n${ex}`),
         updErr:     ex                 => printMsg(`get pi updates:\n${ex}`),
