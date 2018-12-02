3c8aa502d979abc2fe83952d8b8a3a5164523438
k03mad <k03.mad@gmail.com>
2018-12-03 01:38:27 +0300

    ntba_fix

--- env.js
+++ env.js
@@ -1,5 +1,8 @@
 'use strict';
 
+// remove "Automatic enabling of cancellation of promises is deprecated"
+process.env.NTBA_FIX_319 = true;
+
 module.exports = {
     yandex: {
         login: process.env.YANDEX_LOGIN,
