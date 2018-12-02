b643ea5947e86ae72c82165b22c850f7ffe68d52
k03mad <k03.mad@gmail.com>
2018-12-03 02:47:45 +0300

    lst push add msg

--- app/cmd/dns/lastpush.js
+++ app/cmd/dns/lastpush.js
@@ -2,7 +2,11 @@
 
 const {shell} = require('utils-mad');
 
-module.exports = () => shell.run([
-    'cd ~/git/adblock-hosts-list',
-    'git show -p origin/master | grep Date',
-]).catch(err => err);
+module.exports = async () => {
+    const date = await shell.run([
+        'cd ~/git/adblock-hosts-list',
+        'git show -p origin/master | grep Date',
+    ]).catch(err => err);
+
+    return `adblock-hosts-list last push\n${date}`;
+};
