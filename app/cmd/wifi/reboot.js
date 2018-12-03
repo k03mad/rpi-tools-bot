1c1670afae33e42a6a01381249993f7998dcfdd7
k03mad <k03.mad@gmail.com>
2018-12-03 18:15:24 +0300

    remove debug

--- app/cmd/wifi/reboot.js
+++ app/cmd/wifi/reboot.js
@@ -6,7 +6,7 @@ const {router} = require('../../../env');
 
 module.exports = async () => {
     try {
-        await request.got('http://19.168.1.133/cgi-bin/timepro.cgi', {
+        await request.got('http://192.168.1.133/cgi-bin/timepro.cgi', {
             query: {
                 tmenu: 'background',
                 smenu: 'reboot',
