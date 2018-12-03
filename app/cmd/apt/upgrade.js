96520abab31c6ffec134c682b8907f2c937786be
Kirill Molchanov <k03.mad@gmail.com>
2018-12-03 21:13:35 +0300

    Update upgrade.js

--- app/cmd/apt/upgrade.js
+++ app/cmd/apt/upgrade.js
@@ -5,7 +5,7 @@ const {shell} = require('utils-mad');
 
 module.exports = () => shell.run([
     'sudo apt-get update',
-    'sudo apt-get upgrade -y',
+    'sudo apt-get dist-upgrade -y',
     'sudo apt-get autoremove -y',
     'sudo apt-get autoclean',
 ]).catch(err => printMsg(err));
