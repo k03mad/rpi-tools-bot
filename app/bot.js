6a82769b6e42d509e5d5932196ffddbe78948d46
k03mad <k03.mad@gmail.com>
2019-02-12 19:40:14 +0300

    replies to obj

--- app/bot.js
+++ app/bot.js
@@ -18,22 +18,28 @@ const bot = new TelegramBot(telegramToken, {
     },
 });
 
-reply(bot, 'app_log', b.app.log);
+const replies = {
+    app_log: b.app.log,
 
-reply(bot, 'apt_update', b.apt.update);
-reply(bot, 'apt_upgrade', b.apt.upgrade);
+    apt_update: b.apt.update,
+    apt_upgrade: b.apt.upgrade,
 
-reply(bot, 'dns_check', b.dns.check);
-reply(bot, 'dns_lastpush', b.dns.lastpush);
-reply(bot, 'dns_query', b.dns.query);
-reply(bot, 'dns_update', b.dns.update);
+    dns_check: b.dns.check,
+    dns_lastpush: b.dns.lastpush,
+    dns_query: b.dns.query,
+    dns_update: b.dns.update,
 
-reply(bot, 'mus_dups', b.mus.dups);
-reply(bot, 'mus_unavail', b.mus.unavail);
+    mus_dups: b.mus.dups,
+    mus_unavail: b.mus.unavail,
 
-reply(bot, 'pi_reboot', b.pi.reboot);
-reply(bot, 'pi_shutdown', b.pi.shutdown);
+    pi_reboot: b.pi.reboot,
+    pi_shutdown: b.pi.shutdown,
 
-reply(bot, 'wifi_reboot', b.wifi.reboot);
+    wifi_reboot: b.wifi.reboot,
+};
+
+for (const [key, value] of Object.entries(replies)) {
+    reply(bot, key, value);
+}
 
 bot.on('polling_error', error => log.print(error));
