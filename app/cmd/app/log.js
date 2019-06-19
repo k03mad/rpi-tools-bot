c5c7c517a21a56266fbd134dfbd5b6cf9f52f4cb
k03mad <k03.mad@gmail.com>
2019-06-19 15:12:40 +0300

    update log

--- app/cmd/app/log.js
+++ app/cmd/app/log.js
@@ -5,12 +5,15 @@ const fs = require('fs');
 const {promisify} = require('util');
 
 const readFile = promisify(fs.readFile);
+const repo = ['rpi-tools-bot', 'rpi-tools-cron', 'grammar-tlgrm-bot'];
 
 module.exports = async () => {
-    const forever = await Promise.all([
-        readFile(`${appRoot}/forever.log`),
-        readFile(`${appRoot}/../rpi-tools-cron/forever.log`),
-    ]);
+    const forever = await Promise.all(
+        repo.map(async elem => {
+            const log = await readFile(`${appRoot}/../${elem}/forever.log`);
+            return `${elem}\n\n${log.toString()}`;
+        }),
+    );
 
-    return forever.map(elem => elem.toString()).join('\n---------\n');
+    return forever.join('\n---------\n');
 };
