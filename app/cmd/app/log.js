b2ee153f2e2dbb8241ef76fbc1043d93ff1c232c
k03mad <k03.mad@gmail.com>
2019-07-05 21:56:37 +0300

    pm2

--- app/cmd/app/log.js
+++ app/cmd/app/log.js
@@ -10,7 +10,7 @@ const repo = ['rpi-tools-bot', 'rpi-tools-cron'];
 module.exports = async () => {
     const forever = await Promise.all(
         repo.map(async elem => {
-            const log = await readFile(`${appRoot}/../${elem}/forever.log`);
+            const log = await readFile(`${appRoot}/../${elem}/pm2.log`);
             return `${elem}\n\n${log.toString()}`;
         }),
     );
