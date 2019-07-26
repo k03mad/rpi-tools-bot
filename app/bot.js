708efca28c7b30eaeeadd66116357727936e6a14
k03mad <k03.mad@gmail.com>
2019-07-27 01:44:36 +0300

    exit on polling err

--- app/bot.js
+++ app/bot.js
@@ -5,7 +5,7 @@ const bot = require('./telegram/config');
 const {reply} = require('./telegram/chat');
 
 Object.entries(all).forEach(([folder, cmds]) => {
-    Object.keys(cmds).forEach(cmd => {
-        reply(bot, `${folder}_${cmd}`, all[folder][cmd]);
-    });
+    Object.keys(cmds).forEach(cmd => reply(bot, `${folder}_${cmd}`, all[folder][cmd]));
 });
+
+bot.on('polling_error', err => print.ex(err, {exit: true}));
