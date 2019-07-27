dfbd84387444fcd6e6dd534daa0b0c525816b2c1
k03mad <k03.mad@gmail.com>
2019-07-27 18:34:32 +0300

    sml fix

--- app/cmd/pm/restart.js
+++ app/cmd/pm/restart.js
@@ -1,13 +1,5 @@
 'use strict';
 
-const flush = require('./flush');
 const {shell} = require('utils-mad');
 
-module.exports = async () => {
-    const logs = [];
-    logs.push(
-        await shell.run('pm2 restart all'),
-        await flush(),
-    );
-    return logs;
-};
+module.exports = () => shell.run('pm2 restart all');
--- app/cmd/tools/update.js
+++ app/cmd/tools/update.js
@@ -1,11 +1,9 @@
 'use strict';
 
-const restart = require('../pm/restart');
 const {repo} = require('utils-mad');
 
 module.exports = async () => [
     await repo.update('rpi-tools-bot'),
     await repo.update('rpi-tools-cron'),
     await repo.update('magnet-server'),
-    await restart(),
 ];
