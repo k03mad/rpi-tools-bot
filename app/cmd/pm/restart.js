092e5481d13452a7a4efa12566b66e3be2a45b30
k03mad <k03.mad@gmail.com>
2019-07-27 19:32:12 +0300

    fix restart

--- app/cmd/pm/restart.js
+++ app/cmd/pm/restart.js
@@ -2,7 +2,10 @@
 
 const {shell} = require('utils-mad');
 
-module.exports = () => {
-    shell.run('sleep 5 && pm2 restart all');
+module.exports = async () => {
+    await shell.run('pm2 restart cron');
+    await shell.run('pm2 restart magnet-server');
+    shell.run('sleep 5 && pm2 restart bot');
+
     return 'restarting...';
 };
