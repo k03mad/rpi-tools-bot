44141d025becfacc9fa2c9e5be7ea0df7a5afc0f
k03mad <k03.mad@gmail.com>
2019-11-08 19:38:37 +0300

    update dns deploy

--- app/cmd/dns/update.js
+++ app/cmd/dns/update.js
@@ -2,7 +2,11 @@
 
 const {repo} = require('utils-mad');
 
-module.exports = () => repo.run(
-    'adblock-hosts-list',
-    'deploy',
-);
+module.exports = async () => {
+    const logs = [];
+
+    logs.push(await repo.run('adblock-hosts-list', 'deploy'));
+    logs.push(await repo.run('rpi-tools-cron', 'task dns update'));
+
+    return logs.join('\n\n');
+};
