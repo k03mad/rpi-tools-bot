3664669e078132f05a4f3a1b7bdb6d532bb5e1b1
k03mad <k03.mad@gmail.com>
2020-01-05 00:01:29 +0300

    update pi switch

--- app/cmd/mik/nat_pi_switch.js
+++ app/cmd/mik/nat_pi_switch.js
@@ -4,10 +4,10 @@ const {mikrotik} = require('utils-mad');
 
 module.exports = async () => {
     const [nat] = await mikrotik.get(['/ip/firewall/nat/print']);
-    const pihole = nat.filter(elem => elem['log-prefix'] === 'pihole');
-    const ids = pihole.map(elem => elem['.id']);
+    const pi = nat.filter(elem => !elem.comment || elem.comment.startsWith('pi'));
+    const ids = pi.map(elem => elem['.id']);
 
-    const status = pihole[0].disabled === 'false' ? 'disable' : 'enable';
+    const status = pi[0].disabled === 'false' ? 'disable' : 'enable';
     await mikrotik.get([...ids.map(id => [`/ip/firewall/nat/${status}`, `=.id=${id}`])]);
 
     return `${status}d`;
