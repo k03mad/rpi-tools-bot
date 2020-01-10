69fa02b45e9f0e2a3dc6a2d216984370d6e5dd63
k03mad <k03.mad@gmail.com>
2020-01-11 00:41:22 +0300

    polling true

--- app/telegram/config.js
+++ app/telegram/config.js
@@ -3,9 +3,4 @@
 const TelegramBot = require('node-telegram-bot-api');
 const {telegramToken} = require('../../env');
 
-module.exports = new TelegramBot(telegramToken, {
-    polling: {
-        interval: 3000,
-        params: {allowed_updates: ['message']},
-    },
-});
+module.exports = new TelegramBot(telegramToken, {polling: true});
