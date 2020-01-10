4be1de8a89c40ebef7ede0e4e7e7b7f1f8e0cbf2
k03mad <k03.mad@gmail.com>
2020-01-11 01:29:10 +0300

    return polling

--- app/telegram/config.js
+++ app/telegram/config.js
@@ -5,6 +5,7 @@ const TelegramBot = require('node-telegram-bot-api');
 const {proxy, telegramToken} = require('../../env');
 
 module.exports = new TelegramBot(telegramToken, {
+    polling: true,
     request: {
         agentClass: Agent,
         agentOptions: proxy,
