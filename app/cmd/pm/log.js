65c2d996ed762e1f527f6c2b72ae35d58cc98bfe
k03mad <k03.mad@gmail.com>
2020-02-16 02:20:12 +0300

    log fix

--- app/cmd/pm/log.js
+++ app/cmd/pm/log.js
@@ -2,4 +2,7 @@
 
 const {shell} = require('utils-mad');
 
-module.exports = () => shell.run('pm2 logs --nostream --lines 100');
+module.exports = async () => {
+    const log = await shell.run('pm2 logs --nostream --lines 100');
+    return log.replace(/\[TAILING][\S\s]+\n\n/, '');
+};
