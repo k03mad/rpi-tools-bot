8e04ecd77a525ffabda4281855cf2294e03c48c9
k03mad <k03.mad@gmail.com>
2020-02-18 22:04:37 +0300

    update logs re

--- app/cmd/pm/log.js
+++ app/cmd/pm/log.js
@@ -4,5 +4,5 @@ const {shell} = require('utils-mad');
 
 module.exports = async () => {
     const log = await shell.run('pm2 logs --nostream --lines 100');
-    return log.replace(/\[TAILING][\S\s]+\n\n/, '');
+    return log.replace(/\[TAILING][\S\s]+?\n\n/, '');
 };
