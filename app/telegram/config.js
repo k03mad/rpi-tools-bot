c9c587f3932d4d626bbb087345755e0ceec884d1
k03mad <k03.mad@gmail.com>
2020-02-27 23:32:07 +0300

    :kiss: deps update

--- app/telegram/config.js
+++ app/telegram/config.js
@@ -1,13 +1,6 @@
 'use strict';
 
-const Agent = require('socks5-https-client/lib/Agent');
 const TelegramBot = require('node-telegram-bot-api');
-const {proxy, telegramToken} = require('../../env');
+const {telegramToken} = require('../../env');
 
-module.exports = new TelegramBot(telegramToken, {
-    polling: true,
-    request: {
-        agentClass: Agent,
-        agentOptions: proxy,
-    },
-});
+module.exports = new TelegramBot(telegramToken, {polling: true});
