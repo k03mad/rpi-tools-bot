a094a047febbd0e6593624e02d9bf63f523804b6
k03mad <k03.mad@gmail.com>
2020-02-27 23:55:00 +0300

    Revert ":kiss: deps update"
    This reverts commit c9c587f3932d4d626bbb087345755e0ceec884d1.

--- env.js
+++ env.js
@@ -1,6 +1,12 @@
 'use strict';
 
 module.exports = {
+    proxy: {
+        socksHost: process.env.PROXY_HOST,
+        socksPort: process.env.PROXY_PORT,
+        socksUsername: process.env.PROXY_USER,
+        socksPassword: process.env.PROXY_PASS,
+    },
     chat: Number(process.env.MY_CHAT),
     telegramToken: process.env.RP_TELEGRAM_TOKEN,
 };
