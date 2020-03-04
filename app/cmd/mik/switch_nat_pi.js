6825305f44a89abf9b2b2e06c2cbc0fdda16edfe
k03mad <k03.mad@gmail.com>
2020-03-04 13:39:34 +0300

    :eyeglasses: fix mikrotik cmds

--- app/cmd/mik/switch_nat_pi.js
+++ app/cmd/mik/switch_nat_pi.js
@@ -3,12 +3,12 @@
 const {mikrotik} = require('utils-mad');
 
 module.exports = async () => {
-    const [nat] = await mikrotik.get(['/ip/firewall/nat/print']);
+    const nat = await mikrotik.write('/ip/firewall/nat/print');
     const pi = nat.filter(elem => !elem.comment || elem.comment.startsWith('pi'));
     const ids = pi.map(elem => elem['.id']);
 
     const status = pi[0].disabled === 'false' ? 'disable' : 'enable';
-    await mikrotik.get([...ids.map(id => [`/ip/firewall/nat/${status}`, `=.id=${id}`])]);
+    await mikrotik.write([...ids.map(id => [`/ip/firewall/nat/${status}`, `=.id=${id}`])]);
 
     return `${status}d`;
 };
