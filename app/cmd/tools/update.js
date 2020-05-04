dc6c03197c1a35f2b55d01785980d83ade272c1f
k03mad <k03.mad@gmail.com>
2020-05-04 22:35:47 +0300

    return npm

--- app/cmd/tools/update.js
+++ app/cmd/tools/update.js
@@ -4,13 +4,18 @@ const restart = require('../pm/restart');
 const {repo} = require('utils-mad');
 
 module.exports = async () => {
+    const logs = [];
+
     const apps = [
         'rpi-tools-bot', 'rpi-tools-cron', 'magnet-co-parser',
         'mikrotik-pptp-hidemy-ip', 'utils-mad',
         'adguard-home-lists-my',
     ];
 
-    const logs = await Promise.all(apps.map(app => repo.update(app)));
+    for (const app of apps) {
+        logs.push(await repo.update(app));
+    }
+
     logs.push(await restart());
 
     return logs;
