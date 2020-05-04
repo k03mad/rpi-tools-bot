202e69a11e42616a1144daf9aaee5b4e59e2f2ce
k03mad <k03.mad@gmail.com>
2020-05-04 21:50:45 +0300

    update deps

--- app/telegram/errors.js
+++ app/telegram/errors.js
@@ -2,13 +2,15 @@
 
 const {print} = require('utils-mad');
 
+const MIN_ERRORS = 30;
+
 module.exports = bot => {
     let pollingErrors = 0;
 
     bot.on('polling_error', err => {
         pollingErrors++;
 
-        if (pollingErrors >= 10) {
+        if (pollingErrors >= MIN_ERRORS) {
             print.ex(err, {
                 before: `onPollingErr (count: ${pollingErrors})`,
                 exit: true,
