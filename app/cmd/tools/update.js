3be286df7d66bf4b6378f247e7c942b89d201d59
k03mad <k03.mad@gmail.com>
2020-06-29 16:55:38 +0300

    add global pkg update

--- app/cmd/tools/update.js
+++ app/cmd/tools/update.js
@@ -1,20 +1,29 @@
 'use strict';
 
 const restart = require('../pm/restart');
-const {repo} = require('utils-mad');
+const {repo, shell} = require('utils-mad');
 
 /** @returns {Promise<Array>} */
 module.exports = async () => {
     const logs = [];
 
     const apps = [
-        'rpi-tools-bot', 'rpi-tools-cron', 'magnet-co-parser',
-        'adguard-home-lists-my',
+        'rpi-tools-bot', 'rpi-tools-cron',
+        'magnet-co-parser', 'adguard-home-lists-my',
     ];
 
     for (const app of apps) {
-        logs.push(await repo.update(app));
+        logs.push(
+            `>>> ${app} <<<`,
+            await repo.update(app),
+        );
     }
+
+    logs.push(
+        '>>> global <<<',
+        // eslint-disable-next-line no-template-curly-in-string
+        await shell.run('for package in $(npm -g outdated --parseable --depth=0 | cut -d: -f4); do npm i ${package} -g; done'),
+    );
 
     logs.push(await restart());
 
