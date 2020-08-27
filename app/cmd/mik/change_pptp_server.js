40399fe62d72cba8aa1639addbf679e3ee3433cd
Kirill Molchanov <k03.mad@gmail.com>
2020-08-27 20:08:47 +0300

    rename cmd

--- app/cmd/mik/change_pptp_server.js
+++ app/cmd/mik/change_pptp_server.js
@@ -5,7 +5,7 @@ const {shell, print} = require('utils-mad');
 /** @returns {Promise<string>} */
 module.exports = () => {
     shell
-        .run('mik-pptp')
+        .run('mad-mik-pptp')
         .catch(err => print.ex(err, {exit: true}));
 
     return 'Change pptp script started';
