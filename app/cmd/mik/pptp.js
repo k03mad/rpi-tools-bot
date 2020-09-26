0b65a0e1642f29272aa947d55390741a7c78208d
k03mad <k03.mad@gmail.com>
2020-09-26 05:19:53 +0300

    add country arg

--- app/cmd/mik/pptp.js
+++ app/cmd/mik/pptp.js
@@ -2,10 +2,13 @@
 
 const {shell, print} = require('utils-mad');
 
-/** @returns {Promise<string>} */
-module.exports = () => {
+/**
+ * @param {string} opts
+ * @returns {Promise<string>}
+ */
+module.exports = opts => {
     shell
-        .run('mad-mik-pptp')
+        .run(`mad-mik-pptp${opts ? ` ${opts}` : ''}`)
         .catch(err => print.ex(err, {exit: true}));
 
     return 'Change pptp script started';
