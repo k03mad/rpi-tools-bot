afc808b4e32ad60552dd09bf01b6426b163e476e
k03mad <k03.mad@gmail.com>
2020-10-29 22:34:42 +0300

    return polling

--- app/telegram/config.js
+++ app/telegram/config.js
@@ -1,26 +1,6 @@
 'use strict';
 
-// https://github.com/yagop/node-telegram-bot-api/issues/319
-process.env.NTBA_FIX_319 = true;
-
-const path = require('path');
 const TelegramBot = require('node-telegram-bot-api');
-const {print} = require('utils-mad');
-const {telegram, mikrotik, pi} = require('../../env');
+const {telegram} = require('../../env');
 
-const bot = new TelegramBot(telegram.token, {
-    webHook: {
-        port: telegram.port,
-        key: path.join(pi.cert, mikrotik.cloud, 'privkey.pem'),
-        cert: path.join(pi.cert, mikrotik.cloud, 'fullchain.pem'),
-    },
-});
-
-bot
-    .setWebHook(`https://${mikrotik.cloud}:${telegram.port}/bot${telegram.token}`)
-    .catch(err => print.ex(err, {
-        before: 'setWebHookErr',
-        exit: true,
-    }));
-
-module.exports = bot;
+module.exports = new TelegramBot(telegram.token, {polling: true});
--- env.js
+++ env.js
@@ -4,12 +4,5 @@ module.exports = {
     telegram: {
         chat: Number(process.env.TELEGRAM_MY_CHAT),
         token: process.env.TELEGRAM_RASPI_TOKEN,
-        port: process.env.TELEGRAM_RASPI_PORT,
-    },
-    mikrotik: {
-        cloud: process.env.MIKROTIK_CLOUD,
-    },
-    pi: {
-        cert: process.env.PI_CERT_FOLDER,
     },
 };
