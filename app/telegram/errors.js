24fa596502e0af02b68e9d66da94a23237b30bf3
k03mad <k03.mad@gmail.com>
2020-11-03 02:39:29 +0300

    return polling err count

--- app/telegram/errors.js
+++ app/telegram/errors.js
@@ -2,12 +2,24 @@
 
 const {print} = require('utils-mad');
 
+const MIN_ERRORS = 30;
+
 /** @param {object} bot */
 module.exports = bot => {
-    bot.on('webhook_error', err => print.ex(err, {
-        before: 'onWebHookErr',
-        exit: true,
-    }));
+    let pollingErrors = 0;
+
+    bot.on('polling_error', err => {
+        pollingErrors++;
+
+        if (pollingErrors >= MIN_ERRORS) {
+            print.ex(err, {
+                before: `onPollingErr (count: ${pollingErrors})`,
+                exit: true,
+            });
+
+            pollingErrors = 0;
+        }
+    });
 
     bot.on('uncaughtException', err => print.ex(err, {
         before: 'onUncaughtEx',
