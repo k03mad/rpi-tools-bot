8be7dd94237d0fd97afadc0278d9bb2c3ac11cbb
k03mad <k03.mad@gmail.com>
2020-12-13 22:00:15 +0300

    :ram: update deps

--- app/cmd/next/logs.js
+++ app/cmd/next/logs.js
@@ -8,25 +8,25 @@ const {next, hosts} = require('utils-mad');
  * @param {number} pageRequests
  * @returns {Promise}
  */
-module.exports = async (domains, sort = 'off', pageRequests = 20) => {
+module.exports = async (domains = '-', sort = 'off', pageRequests = 20) => {
     if (!domains) {
-        return '/next_logs {type (-|+|all)} {sort (on|off)} {pages (N)}';
+        return '/next_logs {type (-|+)} {sort (on|off)} {pages (N)}';
     }
 
     const sortDomains = domainsSet => sort === 'on'
         ? hosts.comment(hosts.sort(new Set(domainsSet))).join('\n')
-        : [...new Set(domainsSet)].join('\n');
+        : [...new Set(domainsSet)].reverse().join('\n');
 
     pageRequests = Number(pageRequests);
 
-    const all = [];
     const allowed = [];
     const blocked = [];
 
     let lastTime;
-    let method = 'push';
 
     for (let i = 1; i <= pageRequests; i++) {
+        let method = 'push';
+
         if (i === pageRequests) {
             lastTime = '';
         }
@@ -45,25 +45,18 @@ module.exports = async (domains, sort = 'off', pageRequests = 20) => {
             method = 'unshift';
         }
 
-        // eslint-disable-next-line no-loop-func
         logs.forEach(({status, name}) => {
-            if (status === 2) {
-                all[method](`- ${name}`);
-                blocked[method](name);
-            } else {
-                all[method](`+ ${name}`);
-                allowed[method](name);
-            }
+            status === 2
+                ? blocked[method](name)
+                : allowed[method](name);
         });
 
         lastTime = logs[logs.length - 1].timestamp;
     }
 
-    if (domains === '-') {
-        return sortDomains(blocked);
-    } else if (domains === '+') {
+    if (domains === '+') {
         return sortDomains(allowed);
     }
 
-    return sortDomains(all);
+    return sortDomains(blocked);
 };
