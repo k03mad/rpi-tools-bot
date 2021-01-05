a749baa0adeacb5a5c2c53e0392dc7101a128386
k03mad <k03.mad@gmail.com>
2021-01-05 18:24:55 +0300

    use next

--- app/cmd/next/check.js
+++ app/cmd/next/check.js
@@ -1,30 +1,44 @@
 'use strict';
 
+const env = require('../../../env');
 const hexyjs = require('hexyjs');
 const pMap = require('p-map');
-const {cyan, dim, yellow, magenta} = require('colorette');
-const {mikrotik} = require('../../../env');
-const {next, request, shell, promise} = require('utils-mad');
+const {cyan, dim, yellow} = require('colorette');
+const {next, request, promise, hosts} = require('utils-mad');
 
 const getList = async path => {
     const list = await next.query({path});
     return list.map(({domain}) => domain);
 };
 
+const prepareAnswer = (domain, Answer) => Answer
+    ? `— ${domain} ${dim(Answer
+        .filter(elem => elem.data.match(/(\d{1,3}\.){3}\d{1,3}/))
+        .map(elem => elem.data)
+        .sort()
+        .pop())}`
+    : `!! ${domain} ${dim('no answer')}`;
+
+const pushRecords = (message, arr, name) => {
+    if (arr.length > 0) {
+        message.push(
+            '',
+            cyan(`${name}:`),
+            hosts.sort(arr),
+        );
+    }
+};
+
 /** @returns {Promise} */
 module.exports = async () => {
-    const ip = /(\d{1,3}\.){3}\d{1,3}/;
-    const noAnswer = dim('no answer');
-
     const lists = ['allowlist', 'denylist'];
 
-    const concurrency = 2;
-    const pause = 5000;
+    const concurrency = 5;
+    const pause = 3000;
 
     const message = [];
 
     for (const list of lists) {
-        await shell.run('mad-mik-dns flush');
         const domains = (await getList(list)).flat();
 
         if (domains.length > 0) {
@@ -37,40 +51,37 @@ module.exports = async () => {
 
             await promise.delay(pause);
 
-            // get dns records: from other doh-service and from current machine dig
-            const [doh, dig] = await Promise.all([
-                pMap(domains, async domain => {
-                    const {Answer} = await request.doh(domain);
-                    return Answer
-                        ? `— ${domain} ${dim(Answer
-                            .filter(elem => elem.data.match(ip))
-                            .map(elem => elem.data)
-                            .sort()
-                            .pop())}`
-                        : `!! ${domain} ${noAnswer}`;
-                }, {concurrency}),
+            // get dns records
+            const cloudflare = [];
+            const nextdns = [];
+            const common = [];
 
-                pMap(domains, async domain => {
-                    const log = await shell.run(`dig @${mikrotik.host} ${domain} +short`);
-                    return log
-                        ? `— ${domain} ${dim(log
-                            .split('\n')
-                            .filter(elem => elem.match(ip))
-                            .sort()
-                            .pop())}`
-                        : `!! ${domain} ${noAnswer}`;
-                }, {concurrency}),
-            ]);
+            await pMap(domains, async domain => {
+                let Answer;
+
+                ({Answer} = await request.doh({domain}));
+                const preparedDef = prepareAnswer(domain, Answer);
+
+                ({Answer} = await request.doh({domain, resolver: `https://dns.nextdns.io/${env.next.config}/Mad-Checker`}));
+                const preparedNext = prepareAnswer(domain, Answer);
+
+                if (preparedDef === preparedNext) {
+                    common.push(preparedDef);
+                } else {
+                    cloudflare.push(preparedDef);
+                    nextdns.push(preparedNext);
+                }
+            }, {concurrency});
 
             await promise.delay(pause);
 
-            // get last requsts logs
+            // get last requests logs
             const {logs} = await next.query({
                 path: 'logs',
                 searchParams: {simple: 1, lng: 'en'},
             });
 
-            // save list from allow/block reasons
+            // save allow/block reasons
             const foundInLists = [];
 
             domains.forEach(domain => {
@@ -91,21 +102,12 @@ module.exports = async () => {
             message.push(
                 '',
                 yellow(`__${list.toUpperCase()}__`),
-                '',
-                cyan('DOH: '),
-                doh,
-                '',
-                cyan('dig: '),
-                dig,
             );
 
-            if (foundInLists.length > 0) {
-                message.push(
-                    '',
-                    magenta('REASONS:'),
-                    foundInLists,
-                );
-            }
+            pushRecords(message, cloudflare, 'cloudflare');
+            pushRecords(message, nextdns, 'nextdns');
+            pushRecords(message, common, 'common');
+            pushRecords(message, foundInLists, 'reasons');
         }
     }
 
