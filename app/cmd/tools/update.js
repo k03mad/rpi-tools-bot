cc7f36131624acc088512ba74d70d42debb93695
k03mad <k03.mad@gmail.com>
2021-01-23 00:18:23 +0300

    rem pnpm

--- app/cmd/tools/update.js
+++ app/cmd/tools/update.js
@@ -14,21 +14,19 @@ module.exports = async () => {
     ];
 
     for (const app of repoUpdate) {
-        const log = await repo.update(app);
-        logs.push(`>>> ${app} <<<`, log);
+        logs.push(
+            `>>> ${app} <<<`,
+            await repo.update(app),
+        );
     }
 
-    // eslint-disable-next-line no-template-curly-in-string
-    const npm = await shell.run('for package in $(npm -g outdated --parseable --depth=0 | cut -d: -f4); do if [[ ! ${package} =~ ^npm@ ]]; then echo "➡️  ${package}" && npm i -g ${package}; fi; done');
-    const pnpm = await shell.run('pnpm i $(pnpm outdated -g --no-table | cut -d\' \' -f 3 | tr \'\n\' \'@\' | sed -r \'s/@@|@$/ /g\') -g');
-
     logs.push(
-        '>>> npm <<<', npm,
-        '>>> pnpm <<<', pnpm,
+        '>>> global <<<',
+        // eslint-disable-next-line no-template-curly-in-string
+        await shell.run('for package in $(npm -g outdated --parseable --depth=0 | cut -d: -f4); do if [[ ! ${package} =~ ^npm@ ]]; then echo "➡️  ${package}" && npm i -g ${package}; fi; done')
+            || 'no updates',
+        await restart(),
     );
-
-    const log = await restart();
-    logs.push('>>> restart <<<', log);
 
     return logs;
 };
