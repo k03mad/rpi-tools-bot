e2320291ca30595a31d68cc3364b64c27b5d7478
k03mad <k03.mad@gmail.com>
2021-01-26 20:23:29 +0300

    debug

--- app/cmd/tools/update.js
+++ app/cmd/tools/update.js
@@ -5,28 +5,33 @@ const {shell, repo} = require('utils-mad');
 
 /** @returns {Promise} */
 module.exports = async () => {
-    const logs = [];
+    // const logs = [];
 
-    const repoUpdate = [
-        'rpi-tools-bot',
-        'rpi-tools-cron',
-        'magnet-co-parser',
-    ];
+    // const repoUpdate = [
+    //     'rpi-tools-bot',
+    //     'rpi-tools-cron',
+    //     'magnet-co-parser',
+    // ];
 
-    for (const app of repoUpdate) {
-        logs.push(
-            `>>> ${app} <<<`,
-            await repo.update(app),
-        );
-    }
+    // for (const app of repoUpdate) {
+    //     logs.push(
+    //         `>>> ${app} <<<`,
+    //         await repo.update(app),
+    //     );
+    // }
 
-    logs.push(
-        '>>> global <<<',
-        // eslint-disable-next-line no-template-curly-in-string
-        await shell.run('for package in $(npm -g outdated --parseable --depth=0 | cut -d: -f4); do if [[ ! ${package} =~ ^npm@ ]]; then echo "➡️  ${package}" && npm i -g ${package}; fi; done')
-            || 'no updates',
-    );
+    const outdated = await shell.run('npm -g outdated --parseable --depth=0');
+    console.log(':: -----------------------');
+    console.log(':: > outdated', outdated);
+    console.log(':: -----------------------');
+    // logs.push(
+    //     '>>> global <<<',
+    //     // eslint-disable-next-line no-template-curly-in-string
+    //     await shell.run("npm -g outdated --parseable --depth=0 | pjs --after \"LINES.map(elem => elem.split(':')[3]).filter(elem => elem.startsWith('npm@') === false).join(' ')\" | xargs -t npm i -g")
+    //         || 'no updates',
+    // );
 
-    logs.push(await restart());
-    return logs;
+    // logs.push(await restart());
+    // return logs;
+    return '1';
 };
