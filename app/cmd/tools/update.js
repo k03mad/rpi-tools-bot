8bb059018bac05a5bf627c05d5c683853f395edf
k03mad <k03.mad@gmail.com>
2021-01-26 20:34:26 +0300

    remove debug

--- app/cmd/tools/update.js
+++ app/cmd/tools/update.js
@@ -5,34 +5,35 @@ const {shell, repo} = require('utils-mad');
 
 /** @returns {Promise} */
 module.exports = async () => {
-    // const logs = [];
+    const logs = [];
 
-    // const repoUpdate = [
-    //     'rpi-tools-bot',
-    //     'rpi-tools-cron',
-    //     'magnet-co-parser',
-    // ];
+    const repoUpdate = [
+        'rpi-tools-bot',
+        'rpi-tools-cron',
+        'magnet-co-parser',
+    ];
 
-    // for (const app of repoUpdate) {
-    //     logs.push(
-    //         `>>> ${app} <<<`,
-    //         await repo.update(app),
-    //     );
-    // }
+    for (const app of repoUpdate) {
+        logs.push(
+            `>>> ${app} <<<`,
+            await repo.update(app),
+        );
+    }
 
     const outdated = await shell.run('npm -g outdated --parseable --depth=0');
-    console.log(':: -----------------------');
-    console.log(':: > outdated', outdated.split(/\s+/));
-    console.log(':: -----------------------');
+    const modules = outdated
+        .split(/\s+/)
+        .map(elem => elem.split(':')[3])
+        .filter(elem => !elem.startsWith('npm@'))
+        .join(' ');
 
-    // logs.push(
-    //     '>>> global <<<',
-    //     // eslint-disable-next-line no-template-curly-in-string
-    //     await shell.run('for package in $(npm -g outdated --parseable --depth=0 | cut -d: -f4); do if [[ ! ${package} =~ ^npm@ ]]; then echo "➡️  ${package}" && npm i -g ${package}; fi; done')
-    //         || 'no updates',
-    //     await restart(),
-    // );
+    if (modules) {
+        logs.push(
+            `>>> ${modules} <<<`,
+            await shell.run(`npm i -g ${modules.join(' ')}`),
+        );
+    }
 
-    // return logs;
-    return '1';
+    logs.push(await restart());
+    return logs;
 };
